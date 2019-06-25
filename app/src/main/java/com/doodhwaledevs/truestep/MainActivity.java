3ded07e5c0cc278786380f26d8f29968011e5ac5
iamsrqansari <ansari.sharique.75@gmail.com>
2019-06-25 18:37:15 +0530

    502 bad gateway

--- app/src/main/java/com/doodhwaledevs/truestep/MainActivity.java
+++ app/src/main/java/com/doodhwaledevs/truestep/MainActivity.java
@@ -4,25 +4,30 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -31,47 +36,33 @@ public class MainActivity extends AppCompatActivity {
     private EditText Password;
     private Button Login;
     private TextView ForgotPassword;
-
-    private static final String TAG = "MainActivity";
-
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String URL = "https://vl0dq6qsn3.execute-api.us-east-1.amazonaws.com/dev/employees";
-
-        final RequestQueue requestQueue= Volley.newRequestQueue(this);
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET,
-                URL,
-                null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject Response) {
-                        Log.e("Response",Response.toString());
-
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Response",error.toString());
-            }
-        }
-
-        );
-    requestQueue.add(objectRequest);
 
         Name = (EditText)findViewById(R.id.etUsername);
         Password =(EditText)findViewById(R.id.etPassword);
         Login = (Button)findViewById(R.id.LoginBtn);
         ForgotPassword=(TextView)findViewById(R.id.TvForgotPass);
 
+
+
+
+
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validate(Name.getText().toString(),Password.getText().toString());
+                String data = "{"+
+                        "\"mobileNo\"" + ":\"" + Name.getText().toString() +"\"," +
+                        "\"password\"" + ":\"" + Password.getText().toString() +"\"" +
+                        "}";
+
+                validate(data);
+
             }
         });
 
@@ -85,19 +76,80 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
     }
 
-    private void validate(String userName , String userPassword)
+
+    private void validate(String data)
     {
-        if ((userName.equals("admin")) && (userPassword.equals("admin")))
-        {
+        final String savedata= data;
+        String URL="https://vl0dq6qsn3.execute-api.us-east-1.amazonaws.com/dev/authenticate";
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject objRes=new JSONObject(response);
+                    Toast.makeText(getApplicationContext(),objRes.toString(),Toast.LENGTH_LONG).show();
+
+
+                } catch (JSONException e) {
+                    Toast.makeText(getApplicationContext(),"Server Error",Toast.LENGTH_LONG).show();
+
+                }
+                //Log.i("VOLLEY", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+
+                Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+
+                //Log.v("VOLLEY", error.toString());
+            }
+        }) {
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return savedata == null ? null : savedata.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    //Log.v("Unsupported Encoding while trying to get the bytes", data);
+                    return null;
+                }
+            }
+
+        };
+        requestQueue.add(stringRequest);
+     /*
             Intent intent = new Intent(MainActivity.this , DashboardActivity.class);
             startActivity(intent);
 
-            Toast.makeText(getApplicationContext(),"Welcome to TRUESTEP!",Toast.LENGTH_SHORT).show();
 
-        }
+       AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Exit")
+                .setMessage(a)
+                .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.super.onBackPressed();
+                    }
+                })
+                .setNegativeButton("Cancel",null).setCancelable(false);
+
+        AlertDialog alert = builder.create();
+        alert.show();
+
+
+            Toast.makeText(getApplicationContext(),a,Toast.LENGTH_SHORT).show();
+*/
+
     }
 
 
